e2dcc23e3fb4ccb729413a1771e319ed6d945e95
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-08-26 22:46:32 +0300

    setup project

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+const dateNow = new Date().getFullYear();
+const Footer = () => (
+  <section className="flexStart footer">
+    <div className="flex flex-col gap-12 w-full">
+      <div className="flex items-start flex-col">
+        <Image src="/logo-purple.svg" width={116} height={38} alt="logo" />
+
+        <p className="text-start text-sm font-normal mt-5 max-w-xs">
+          Flexibble is the world&apos;s leading community for creatives to
+          share, grow, and get hired.
+        </p>
+      </div>
+    </div>
+
+    <div className="flexBetween footer_copyright">
+      <p>@ {dateNow} Flexibble. All rights reserved</p>
+      <p className="text-gray">
+        <span className="text-black font-semibold">10,214</span> projects
+        submitted
+      </p>
+    </div>
+  </section>
+);
+
+export default Footer;
