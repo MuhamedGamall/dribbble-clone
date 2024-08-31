3b70ccd550c03e0d34cbaf40e99e6a5a44d8caca
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-09-01 02:27:51 +0300

    upadate

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,9 +2,9 @@ import ProjectsContent from "@/components/ProjectsContent";
 import SearchBar from "@/components/SearchBar";
 import { fetchProjects } from "@/lib/actions";
 
-// export const dynamic = "force-dynamic";
-// export const dynamicParams = true;
-// export const revalidate = 0;
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
 
 const Home = async ({
   searchParams: { q },
