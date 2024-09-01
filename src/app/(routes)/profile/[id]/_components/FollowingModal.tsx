15922c160158a6a3150ede2fb917b82d6059a8ed
JimmyDev <111704783+MuhamedGamall@users.noreply.github.com>
2024-09-01 04:27:45 +0300

    Update FollowingModal.tsx

--- src/app/(routes)/profile/[id]/_components/FollowingModal.tsx
+++ src/app/(routes)/profile/[id]/_components/FollowingModal.tsx
@@ -46,11 +46,13 @@ export default function FollowingModal({
               </div>
             </div>
           }
-          <div className="flex flex-col items-center gap-2 transition-colors hover:bg-accent hover:text-accent-foreground focus:outline-none rounded-md">
+          
+          
+          <div className="flex flex-col  gap-2 transition-colors hover:bg-accent hover:text-accent-foreground focus:outline-none rounded-md">
             {followingData?.length > 0 ? (
               followingData?.map((el) => (
-                <>
-                  <DropdownMenuItem className="flex border-b flex-[3] cursor-pointer items-center gap-2 rounded-md p-1 pr-3 text-sm font-medium ">
+                <div className="flex items-center bordeer-b gap-2 rounded-md">
+                  <DropdownMenuItem className="flex flex-[3] cursor-pointer items-center gap-2 rounded-md p-1 pr-3 text-sm font-medium ">
                     <Link
                       href={`/profile/${el?._id}`}
                       className="flex items-center gap-2 p-2"
@@ -75,7 +77,7 @@ export default function FollowingModal({
                       Unfollow
                     </button>
                   </ToggleFollowButton>
-                </>
+                </div>
               ))
             ) : (
               <span className="my-10 text-center w-full text-slate-600 font-semibold">
